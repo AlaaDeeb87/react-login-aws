56cd3bf4b913b0ddf00b503fd20a96f51c5711b4
Alaa Deeb <71987914+AlaaDeeb87@users.noreply.github.com>
2022-02-28 15:58:25 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Amplify from 'aws-amplify'
+import config from './aws-exports'
+import '@aws-amplify/ui-react/styles.css'
+import { AmplifyProvider } from '@aws-amplify/ui-react'
+
+Amplify.configure(config)
 
 ReactDOM.render(
-  <React.StrictMode>
+  <AmplifyProvider>
     <App />
-  </React.StrictMode>,
+  </AmplifyProvider>,
   document.getElementById('root')
 );
 
